26f7c3025442eaf261dd7074c13caffc25ce00b8
jsamit <js.amit01@gmail.com>
2024-04-06 18:12:12 +0530

    Revert "Added Catalog service - create & get products"
    This reverts commit c7146f5b4ab978369d8f2db71a6031a7c7dc48fe.

--- src/BuildingBlocks/Core.Common/CQRS/ICommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using MediatR;
-
-namespace Core.Common.CQRS;
-
-public interface ICommand : ICommand<Unit>;
-
-public interface ICommand<out TResponse> : IRequest<TResponse> where TResponse : notnull;
--- src/BuildingBlocks/Core.Common/CQRS/IQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-
-namespace Core.Common.CQRS;
-
-public interface IQuery<TResponse> : IRequest<TResponse>
-    where TResponse : notnull;
